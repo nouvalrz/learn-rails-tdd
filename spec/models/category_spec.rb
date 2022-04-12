05c884adf213defb139be0fa09874f6a4d7cb36c
Nouval Rizky <nouvalr@gmail.com>
2022-04-12 16:58:10 +0000

    Add validation for Category, invalid without name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,15 +9,13 @@ RSpec.describe Category, type: :model do
     expect(category).to be_valid
   end
 
-  # it 'is invalid without a name' do
-  #   food = Food.new(
-  #     name: nil,
-  #     description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-  #     price: 15000.0
-  #   )
+  it 'is invalid without a name' do
+    category = Category.new(
+      name: nil
+    )
 
-  #   food.valid?
+    category.valid?
 
-  #   expect(food.errors[:name]).to include("can't be blank")
-  # end
+    expect(category.errors[:name]).to include("can't be blank")
+  end
 end
